9a7c7b2f498678a997d7b1c73d0ee88951aa3b1c
Jake Tran <jake.tranehs@gmail.com>
2018-04-07 02:04:07 -0700

    style vs inline style

--- React/test-app/src/App.js
+++ React/test-app/src/App.js
@@ -9,7 +9,7 @@ class App extends Component {
 
   state = {
     characters: [
-      { name: 'Zero Two', code: '002' },
+      { name: 'Zero Two' || 'Goro', code: '002' },
       { name: 'Hiro', code: '016' },
       { name: 'Ichigo', code: '015' }
     ]
@@ -27,7 +27,27 @@ class App extends Component {
     })
   }
 
+  nameChangeHandler = (event) => {
+    this.setState({
+      characters: [
+        { name: 'Goro', code: '056' },
+        { name: event.target.value, code: '196' },
+        { name: 'Futoshi', code: '214' }
+      ]
+    })
+  }
+
   render() {
+
+    {/* This is Inline Style */}
+    const style = {
+      backgroundColor: 'white',
+      font: 'inherit',
+      border: '2px solid blue',
+      padding: '8px',
+      cursor: 'pointer'
+    }
+
     return (
       <div className="App">
         <header className="App-header">
@@ -39,11 +59,11 @@ class App extends Component {
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
 
-        <button onClick={() => this.switchNameHandler('Goro')}>Switch Character</button>
+        <button style={style} onClick={() => this.switchNameHandler('Goro')}>Switch Character</button>
 
         <Hello name={this.state.characters[0].name} code={this.state.characters[0].code} click={this.switchNameHandler.bind(this, 'Ruko')} />
-        <Hello name={this.state.characters[1].name} code={this.state.characters[1].code} click={this.switchNameHandler} />
-        <Hello name={this.state.characters[2].name} code={this.state.characters[2].code} click={this.switchNameHandler} />
+        <Hello name={this.state.characters[1].name} code={this.state.characters[1].code} changeName={this.nameChangeHandler} />
+        <Hello name={this.state.characters[2].name} code={this.state.characters[2].code} />
       </div>
     );
   }
--- React/test-app/src/TestCom/Hello.js
+++ React/test-app/src/TestCom/Hello.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import './Hello.css'
 
 const hello = (props) => {
   return (
-    <div>
+    <div className='Character'>
       <h2 onClick={props.click}>This is a functional Component</h2>
       <h2>This prop character is: [{props.name}] Code: [{props.code}]</h2>
+      <input type='text' onChange={props.changeName} value={props.name} />
     </div>
   )
 }
